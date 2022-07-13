19def3eaac50f1c56225e7054164e6d52c5a899e
threadsv1 <majdimed3@gmail.com>
2022-07-13 22:58:50 +0100

    updated

---
+++ api/v1/app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""api link"""
+
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """Close current session"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Return a JSON-formatted 404 """
+    return (jsonify({"error": "Not found"}), 404)
+
+
+if __name__ == "__main__":
+    hosts = getenv('HBNB_API_HOST', default='0.0.0.0')
+    ports = getenv('HBNB_API_PORT', default=5000)
+    app.run(host=hosts, port=ports, threaded=True)
